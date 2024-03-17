e5026ba1ac975e9a13c7291f0688400599f2f593
Dietbrand Tollenaere <dietbrand.tollenaere@gmail.com>
2024-03-17 11:09:28 +0100

    Added notification service

---
+++ src/NotificationService/Program.cs
@@ -0,0 +1,29 @@
+using MassTransit;
+using NotificationService.Consumers;
+using NotificationService.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(x =>
+{
+  x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+  x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
+
+  x.UsingRabbitMq((context, cfg) =>
+  {
+    cfg.Host(builder.Configuration["RabbitMQ:Host"], "/", host =>
+    {
+      host.Username(builder.Configuration.GetValue("RabbitMQ:Username", "guest"));
+      host.Username(builder.Configuration.GetValue("RabbitMQ:Password", "guest"));
+    });
+    cfg.ConfigureEndpoints(context);
+  });
+});
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapHub<NotificationHub>("/notifications");
+
+app.Run();
